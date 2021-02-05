41c17578b6dbe6adddeebf9b19087fd633a5ebac
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-02-05 14:38:22 +0100

    add find me icons

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../../../css/Contact.css';
+import codewars from './../../../images/codewars.png'
+import linkedin from './../../../images/linkedin.png'
+import github from './../../../images/github.png'
 
 class Contact extends React.Component {
     state ={
@@ -106,48 +109,53 @@ class Contact extends React.Component {
 
     render(){
         
+        const link = [
+            {name: "LinkedIn", img: linkedin, link: "https://www.linkedin.com/in/przemekrura/" ,alt: "LinkedIn's logo"},
+            {name: "GitHub", img: github, link: "https://github.com/przemekr95" ,alt: "GitHub's logo"},
+            {name: "CodeWars", img: codewars, link: "https://www.codewars.com/users/przemekr95" ,alt: "CodeWars's logo"}
+        ]
+
+        const links =link.map(item => (
+            <a href={item.link} target="_blank" rel='noreferrer' key={item.name}>
+                <img src={item.img} alt={item.alt}/>
+                <span>{item.name}</span>
+            </a>
+        ))
+
+
     return(
         
         <div className="contact-wrapper">
             <h2>Contact</h2>
             <div className="contact">
                 <div className="contact-form-box">
-
                     <h5>Have a question or want to work together?</h5>
-
-
                     <form className="contact-form">
                         <div className="form-box">
                             <label htmlFor="username">Name:</label>
                             <input id="username" type="text" value={this.state.username} onChange={this.handleChange}/>
                             <p className="error-text">Fill in correctly</p>
                         </div>
-
                         <div className="form-box">
                             <label htmlFor="email">Email:</label>
                             <input id="email" type="email" value={this.state.email} onChange={this.handleChange} />
                             <p className="error-text">Fill in correctly</p>
                         </div>
-
                         <div className="form-box">
                             <label htmlFor="subject">Subject:</label>
                             <input id="subject" type="text" value={this.state.subject} onChange={this.handleChange} />
                             <p className="error-text">Fill in correctly</p>
                         </div>
-                        
                         <div className="form-box">
                             <label htmlFor="message">Message:</label>
                             <textarea id="message" value={this.state.message} onChange={this.handleChange} />
                             <p className="error-text">Fill in correctly</p>
                         </div>
-    
-
-
                         <div className="control-buttons">
-                        <div className="popup">
-                            <p>Message send!</p>
-                            <button className="close" onClick={this.closeBtn}>Close</button>
-                        </div>
+                            <div className="popup">
+                                <p>Message send!</p>
+                                <button className="close" onClick={this.closeBtn}>Close</button>
+                            </div>
                             <button className="clear" onClick={this.clearBtn}>Wyczyść</button>
                             <button className="send" onClick={this.sendBtn}>Wyślij</button>
                         </div>
@@ -162,8 +170,17 @@ class Contact extends React.Component {
 
                 </div>
                 <div className="contact-icons">
-                    <h5>Find me on:</h5>
+                    
                 
+                
+                    <h5>Find me on:</h5>
+                    <div className="icons">
+                        {links}
+                    </div>
+
+                
+
+
                 </div>
             </div>
         </div>
